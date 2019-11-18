1f0cce089e28633287a451d82b442d921c3a3a8e
azertypow <azertypow@gmail.com>
2019-11-19 00:22:36 +0100

    init directory for multi p5.js canvas instances

---
+++ Experiments/BinderyP5--multi-canvas/sketch.js
@@ -0,0 +1,48 @@
+let p5Canvas;
+
+function setup() {
+
+	let p5Id = document.getElementById('p5')
+	p5Canvas = createCanvas(250, 250)
+	p5Canvas.parent(p5Id)
+
+	background(255, 0, 0)
+
+	ellipse(width / 2, height / 2, width, height)
+
+
+}
+
+function keyPressed() {
+	if (key === ' ') {
+
+		let p5Img = select("#p5Image")
+		canvasToImageElement(p5Canvas, p5Img)
+
+		Bindery.makeBook({ content: ".content" })
+	}
+}
+
+/**
+ * Save HTMLCanvasElement content to an img HTMLElement by query selector
+ * @param {p5.Renderer} canvasSelector		- p5 Renderer canvas to convert in to an HTMLImgElement
+ * @param {p5.Element|null} elementSelector - image element selected with select() P5 function
+ * */
+function canvasToImageElement(canvasSelector, elementSelector) {
+
+	const canvasParentElement = canvasSelector.parent()
+
+	if(canvasParentElement instanceof HTMLParagraphElement) {
+		const imgElement = new Image()
+
+		imgElement.width 	= canvasSelector.width
+		imgElement.height 	= canvasSelector.height
+		imgElement.src 		= canvasSelector.elt.toDataURL("image/png")
+
+		canvasParentElement.appendChild(imgElement)
+		canvasParentElement.removeChild(canvasSelector.elt)
+	} else {
+		console.error("p5 canvas elements must be placed in a HTMLParagraphElement parent node:\n<p><canvas id='p5-canvas'></canvas></p>\n", canvasSelector.elt)
+	}
+
+}

123218a544de783ba18f206fa9bcbd5b155b42de
azertypow <azertypow@gmail.com>
2019-11-19 15:08:19 +0100

    feat: implement p5.speech

--- Experiments/BinderyP5--multi-canvas-with-a-main-p5js-sketch/sketch.js
+++ Experiments/BinderyP5--multi-canvas-with-a-main-p5js-sketch/sketch.js
@@ -49,11 +49,34 @@ const p5_sketch_2 = (p5) => { // p5 could be any variable name
 const myP52 = new p5(p5_sketch_2, "p5-container-2")
 
 
+// ==========
 // main sketch
+// ==========
+
+// sample from https://github.com/IDMNYU/p5.js-speech/blob/master/examples/05continuousrecognition.html
+const myRec 			= new p5.SpeechRec('en-US', parseResult); // new P5.SpeechRec object
+myRec.continuous 		= true; // do continuous recognition
+myRec.interimResults 	= true; // allow partial recognition (faster, less accurate)
+
 function setup() {
 	console.info("main p5js sketch:\n", "access to other p5js sketches\n", myP51, "\n", myP52)
+
+	//myRec.onResult = parseResult; // now in the constructor
+	myRec.start(); // start engine
 }
 
 function draw() {
 
+}
+
+function parseResult() {
+	// recognition system will often append words into phrases.
+	// so hack here is to only use the last word:
+	const mostrecentword = myRec.resultString.split(' ').pop()
+
+	if(mostrecentword.indexOf("print")!==-1) {
+		binderyControl.makeBook()
+	}
+
+	console.log(mostrecentword);
 }

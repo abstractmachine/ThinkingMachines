7b5f7ca874b25004b27aa55a2cb3135abcf28ccb
Douglas Edric Stanley <abstractmachine@abstract-macbook.local>
2020-01-12 15:57:33 +0100

    Working example of changing values in Twee from Javascript

--- Experiments/Twee/scripts/script.js
+++ Experiments/Twee/scripts/script.js
@@ -1,11 +1,17 @@
-
+var twee, twee_div;
 
 function start() {
 
 	startSound();
-
 	document.getElementById("start").style.display = "none";
-	document.getElementById("twee").style.display = "block";
 
-	custom.goto('Started');
+	twee = document.getElementById("twee_frame").contentWindow;
+	twee_div = document.getElementById("twee_div");
+	
+	twee_div.style.display = "block";
+
+	var frog = twee.getVariable('frog');
+
+	console.log(frog);
+
 }

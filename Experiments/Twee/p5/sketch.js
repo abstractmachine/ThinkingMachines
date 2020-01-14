b64667cd60d66798b1aa1413ee8eb5c7fd085bd9
Jürg Lehni <juerg@scratchdisk.com>
2020-01-14 19:57:24 +0100

    Fixed parent access problem with iFrame

--- Experiments/Twee/p5/sketch.js
+++ Experiments/Twee/p5/sketch.js
@@ -1,13 +1,11 @@
 // twee/twine access
-let twee
+
+// get access to twee frame
+let twee = document.getElementById("twee_frame").contentWindow
 
 function setup() {
 	// we're not going to draw anything with P5. We'll let Twee do all the visual communication
 	noCanvas()
-	// get access to twee frame
-	twee = document.getElementById("twee_frame").contentWindow
-	// tell twee how to acces this window
-	twee.setParentInstance(this)
 }
 
 // function draw() {
